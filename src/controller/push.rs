9c05d6dce68875f50c7cf70c17aecb80ef1e40ef
brrond <danya17072@gmail.com>
2026-04-20 22:16:23 +0200

    Write HTTP request to push workitems to jira

--- src/controller/push.rs
+++ src/controller/push.rs
@@ -19,5 +19,64 @@ pub fn jira(
         getter::filter_activities(processed_activities, &filter);
     filtered_activities.sort_by_key(|activity| activity.start);
 
+    let client = reqwest::blocking::Client::new();
+
+    filtered_activities.iter().for_each(|activity| {
+        if activity.end.is_none() {
+            println!("Ignoring activity {}, as it's still in progress", activity);
+            return;
+        }
+
+        let issue_key = activity.project.clone();
+        let comment = activity.description.clone();
+        let started = activity.start.format("%Y-%m-%dT%H:%M:%S.000+0200");
+        let time_spent_seconds = (activity.end.unwrap() - activity.start).num_seconds();
+
+        let body = format!(
+            "
+            {{
+                \"comment\": {{
+                    \"content\": [
+                        {{
+                            \"content\": [
+                                {{
+                                    \"text\": \"{comment}\",
+                                    \"type\": \"text\"
+                                }}
+                            ],
+                            \"type\": \"paragraph\"
+                        }}
+                    ],
+                    \"type\": \"doc\",
+                    \"version\": 1
+                }},
+                \"started\": \"{started}\",
+                \"timeSpentSeconds\": {time_spent_seconds}
+            }}
+            "
+        );
+        // println!("Body to send: {}", body);
+        println!(
+            "Trarnsmitting a worklog for {} {}s",
+            issue_key, time_spent_seconds
+        );
+
+        let res = client
+            .post(format!(
+                "{jira_server}/rest/api/3/issue/{issue_key}/worklog"
+            ))
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .body(body)
+            .basic_auth(jira_user, Option::Some(jira_token))
+            .send();
+
+        if res.is_err() || (res.is_ok() && !res.unwrap().status().is_success()) {
+            panic!("Error during pushing to Jira");
+        }
+    });
+
+    println!("All items were successfully uploaded to Jira.");
+
     Ok(())
 }
